161e7443d6406444896f68f41582bce108b602f0
Prashant Verma <prashantverma.vn@gmail.com>
2020-08-15 16:08:29 +0530

    Update Footer.js

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -4,9 +4,9 @@ import {Container} from 'reactstrap';
 
 const Footer = () => {
     return(
-        <Container fluid className="text-center bg-info text-white text-uppercase
-        fixed-bottom mt-3 p-2">
-            github repo search app
+        <Container fluid className="text-center fixed-bottom mt-3 p-2" 
+        style={{backgroundColor: '#DAE0E2'}}>
+            GitSearch | Made by Prashant Verma
         </Container>
     )
 
